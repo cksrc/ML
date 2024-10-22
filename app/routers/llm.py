27a3baaa8bf916c93a7368665cf6142bdb3d776f
Costa K <konstantinos.kakousis@beamtrail.com>
2024-10-22 15:55:56 +0400

    Completed the first tutorial with DecissionRegressor and RandomForestRegressor

--- app/routers/llm.py
+++ app/routers/llm.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sklearn.metrics import mean_absolute_error
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 import pandas as pd
@@ -66,14 +67,53 @@ def read_items() -> dict:
     # Evaluate
     # The error between the predicted and actual home prices is
     mae = mean_absolute_error(val_y, predicted_home_prices)
-    response = {
-        "predictions": predicted_home_prices.tolist(),
-        "mean_absolute_error": mae
-    }
-    #TODO: https://www.kaggle.com/code/elprofe55or/exercise-model-validation/edit
+    
 
+    #Model Fine Tuning
+    # Overfitting: is the situation where a model matches the training data almost perfectly, 
+    # but does poorly in validation and other new data. 
+    # Underfitting: When a model fails to capture important distinctions and patterns in the data, 
+    # so it performs poorly even in training data
+    # Find the best tree size among possible options
+    possible_leaf_nodes = [5, 50, 5000, 5000]
+    leaf_nodes_to_mae = {}
+    for leaf_nodes in possible_leaf_nodes:
+        leaf_nodes_to_mae[leaf_nodes] = get_mae(leaf_nodes, train_X=train_X, val_y=val_y, train_y=train_y, val_X=val_X)
+    
+    best_tree_size = min(leaf_nodes_to_mae, key=leaf_nodes_to_mae.get)
+    
+    
+
+    # The random forest uses many trees, 
+    # and it makes a prediction by averaging the predictions of each component tree. 
+    # It generally has much better predictive accuracy than a single decision tree 
+    # and it works well with default parameters.
+    # There are parameters which allow you to change the performance of the Random Forest
+    # much as we changed the maximum depth of the single decision tree. 
+    # But one of the best features of Random Forest models is that they generally work 
+    # reasonably even without this tuning.
+    forest_model =  RandomForestRegressor(random_state=1)
+    forest_model.fit(X=train_X, y=train_y)
+    random_fores_preds = forest_model.predict(X=val_X)
+    random_forest_mae = mean_absolute_error(y_true=val_y, y_pred=random_fores_preds)
+
+    response = {
+        "predictions": predicted_home_prices[0:5].tolist(),
+        "not_optimized_mean_absolute_error": mae,
+        "optimized_mean_absolute_error": leaf_nodes_to_mae[best_tree_size],
+        "best_tree_size": best_tree_size,
+        "random_forest_mae": random_forest_mae
+
+    }
     return response
 
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
 
 
 
